23476c34782a3738bbaac0b123c4a851994f6dc7
JungaBunga <amir.ishmuhametov@gmail.com>
2024-02-09 12:19:08 +0000

    try number 7

--- resources/postgres-setup-scripts/README.md
+++ resources/postgres-setup-scripts/README.md
@@ -1,4 +1,4 @@
-Below you can find instructions on a recently automated way of running the initial data setup scripts using a single command. 
+Below you can find instructions on a recently automated way of running the initial data setup scripts using a single command for a specified statements/services. The nested folder inside postgres-setup-scripts coorelates with statements we are producing using Documents team services. In future we are planning to add more folders with sql scripts for each type of statement, for example in near future we will add a folder containing sql scripts for SFI-23 statement.
 
 1.To install PostgreSQL on WSL (ie. Ubuntu):
 Open your WSL terminal (ie. Ubuntu).
@@ -6,9 +6,9 @@ Update your Ubuntu packages: sudo apt update
 Once the packages have updated, install PostgreSQL (and the -contrib package which has some helpful utilities) with: sudo apt install postgresql postgresql-contrib
 Confirm installation and get the version number: psql --version
 
-2.Download the script package from repo into your WSL: (link is coming)
+2.Download the script package from repo into your WSL
 
-3.Ensure all services are up and running and navigate to the folder postgres-setup-scripts and issue command : ./automate_queries.sh
+3.Ensure all services are up and running and navigate to the folder postgres-setup-scripts and issue command : ./automate-SFI22.sh
 You should see the execution entries of scripts being logged in your WSL window, there is a success/fail message for each service/script.
 
 Note : execution of scripts is linear, so if one fails, the script will exit giving indicative error on which service has failed and no other sql scripts will be executed.
@@ -22,7 +22,7 @@ Ffc-pay-processing
 ffc-pay-batch-processor
 
 
-Below you can find the anatomy of script expression, so you can add new entries, edit existing ones in case there is a change to database setup or delete them if you are not using them. Current version only supports the above services for Documents team, so please ensure this is tailored to your own services using below schema to edit properties of the script expressions.
+Below you can find the anatomy of script expression, so you can add new entries, edit existing ones in case there is a change to database setup or delete them if you are not using them. Current version only supports the above services for Documents team, so please ensure this is tailored to your own services by using below schema to edit properties of the script expressions.
 
 
 
--- resources/postgres-setup-scripts/automate-SFI22.sh
+++ resources/postgres-setup-scripts/automate-SFI22.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Execute the ffc-pay-statement-data sql scrip
-PGPASSWORD=ppp psql -h host.docker.internal -p 5452 -U postgres -d ffc_pay_statement_data -f $(pwd)/sql_start_scripts/ffc-pay-statement-data.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5452 -U postgres -d ffc_pay_statement_data -f $(pwd)/SFI22-statements-data/ffc-pay-statement-data.sql 
 
 # Check the exit status of the ffc-pay-statement-data sql script
 if [ $? -eq 0 ]; then
@@ -12,7 +12,7 @@ else
 fi
 
 # Execute the ffc-pay-submission sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5438 -U postgres -d ffc_pay_submission -f $(pwd)/sql_start_scripts/ffc-pay-submission.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5438 -U postgres -d ffc_pay_submission -f $(pwd)/SFI22-statements-data/ffc-pay-submission.sql 
 
 # Check the exit status of ffc-pay-submission sql script
 if [ $? -eq 0 ]; then
@@ -23,7 +23,7 @@ else
 fi
 
 # Execute the ffc-pay-statement-generator sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5451 -U postgres -d ffc_pay_statement_generator -f $(pwd)/sql_start_scripts/ffc-pay-statement-generator.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5451 -U postgres -d ffc_pay_statement_generator -f $(pwd)/SFI22-statements-data/ffc-pay-statement-generator.sql 
 
 # Check the exit status of ffc-pay-statement-generator sql script
 if [ $? -eq 0 ]; then
@@ -34,7 +34,7 @@ else
 fi
 
 # Execute the ffc-pay-statement-constructor sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5450 -U postgres -d ffc_pay_statement_constructor -f $(pwd)/sql_start_scripts/ffc-pay-statement-constructor.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5450 -U postgres -d ffc_pay_statement_constructor -f $(pwd)/SFI22-statements-data/ffc-pay-statement-constructor.sql 
 
 # Check the exit status of ffc-pay-statement-constructor sql script
 if [ $? -eq 0 ]; then
@@ -45,7 +45,7 @@ else
 fi
 
 # Execute the ffc-pay-request-editor sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5433 -U postgres -d ffc_pay_request_editor -f $(pwd)/sql_start_scripts/ffc-pay-request-editor.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5433 -U postgres -d ffc_pay_request_editor -f $(pwd)/SFI22-statements-data/ffc-pay-request-editor.sql 
 
 # Check the exit status of ffc-pay-request-editor sql script
 if [ $? -eq 0 ]; then
@@ -56,7 +56,7 @@ else
 fi
 
 # Execute the ffc-pay-processing sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5434 -U postgres -d ffc_pay_processing -f $(pwd)/sql_start_scripts/ffc-pay-processing.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5434 -U postgres -d ffc_pay_processing -f $(pwd)/SFI22-statements-data/ffc-pay-processing.sql 
 
 # Check the exit status of ffc-pay-request-editor sql script
 if [ $? -eq 0 ]; then
@@ -67,7 +67,7 @@ else
 fi
 
 # Execute the ffc-pay-batch-processor sql script
-PGPASSWORD=ppp psql -h host.docker.internal -p 5436 -U postgres -d ffc_pay_batch_processor -f $(pwd)/sql_start_scripts/ffc-pay-batch-processor.sql 
+PGPASSWORD=ppp psql -h host.docker.internal -p 5436 -U postgres -d ffc_pay_batch_processor -f $(pwd)/SFI22-statements-data/ffc-pay-batch-processor.sql 
 
 # Check the exit status of ffc-pay-request-editor sql script
 if [ $? -eq 0 ]; then
